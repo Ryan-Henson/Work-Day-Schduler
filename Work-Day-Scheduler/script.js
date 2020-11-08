9aa638cd663641ab9845f6083acea3c6b6b8c9a8
Ryan-Henson <72321799+Ryan-Henson@users.noreply.github.com>
2020-11-07 23:53:40 -0600

    removed css that study group made and included js code for save button click along with localStorage

--- Work-Day-Scheduler/script.js
+++ Work-Day-Scheduler/script.js
@@ -1,12 +1,45 @@
-let fullDay = {
-  "8 AM": "",
-  "9 AM": "",
-  "10 AM": "",
-  "11 AM": "",
-  "12 PM": "",
-  "1 PM": "",
-  "2 PM": "",
-  "3 PM": "",
-  "4 PM": "",
-  "5 PM": "",
-};
+var date = moment().format("MMM Do YY");
+
+$("#currentDay").append(date);
+
+var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+updatetime();
+
+function updatetime() {
+  var currentTime = moment().format("H");
+  for (var i = 0; i < timeOfday.length; i++) {
+    if (parseInt(timeOfday[i]) > currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: #58ce7b");
+    } else if (parseInt(timeOfday[i]) < currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: lightgray");
+    } else if (parseInt(timeOfday[i]) == currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: #fc665e");
+    }
+  }
+}
+
+$(".rowBtn").on("click", function () {
+  var timeOfday = $(this).parent().attr("id");
+  var textContent = $("input").val().trim();
+
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
+});
+
+$("#9am").children("input").val(localStorage.getItem("9am"));
+
+$("#10am").children("input").val(localStorage.getItem("10am"));
+
+$("#11am").children("input").val(localStorage.getItem("11am"));
+
+$("#12pm").children("input").val(localStorage.getItem("12pm"));
+
+$("#1pm").children("input").val(localStorage.getItem("1pm"));
+
+$("#2pm").children("input").val(localStorage.getItem("2pm"));
+
+$("#3pm").children("input").val(localStorage.getItem("3pm"));
+
+$("#4pm").children("input").val(localStorage.getItem("4pm"));
+
+$("#5pm").children("input").val(localStorage.getItem("5pm"));
